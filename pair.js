d90928335937e0a5cb5dc3348cb94deb7b3eb6bd
Bonioutlook <boniphace_malegesi092@outlook.com>
2024-08-03 23:25:51 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -76,7 +76,7 @@ router.get('/', async (req, res) => {
 
 > 3rd STEP
 *FORK AND STAR 🌟 THE REPO FOR COURAGE*
-𓄂 *_https://github.com/boniphace704/boniphace_md_*
+𓄂 *_https://github.com/boniphace478/BONIPHACE-MD_*
 
 > 4th STEP 
 *VISIT FOR TUTORIALS IF YOU DON'T KNOW HOW TO DEPLOY*
